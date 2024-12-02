f87a6e3180562cfba215f2570b7eed7fb76a2615
Apricityx <apricityx@qq.com>
2024-12-02 14:15:06 +0800

    Feat. Change fetch() to ctx.http.get() Feat. Concurrent fetch memes.

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import {Context, Schema, h} from 'koishi'
 import sharp from 'sharp';
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch';
+import axios from 'axios';
+
 
 export const name = 'nrl-meme'
 
 export interface Config {
   api_url: string,
   debug: boolean,
+  enable_concurrent: boolean,
   enable_convert: boolean,
-  jpgQuality: number,
+  jpg_quality: number
 }
 
 export const Config: Schema = Schema.intersect([
@@ -18,6 +21,7 @@ export const Config: Schema = Schema.intersect([
   }).description('基础配置'),
   Schema.object({
     enable_convert: Schema.boolean().description('是否启用WEBP转JPG再发送（如果遇到奇怪的问题建议开启）').default(false),
+    enable_concurrent: Schema.boolean().description('是否启用并发来加速图片获取').default(false).experimental()
   }).description('高级配置'),
   Schema.union([
     Schema.object({
@@ -33,7 +37,15 @@ export const Config: Schema = Schema.intersect([
 //   jpgQuality: Schema.number().description('JPG图像质量（转JPG开启后生效）').default(90),
 // })
 
+
 export function apply(ctx: Context, config: Config) {
+  const getBase64Image = async () => {
+    const webpBuffer = await ctx.http.get(getRandomUrl())
+    const jpgBuffer = await sharp(Buffer.from(webpBuffer))
+      .jpeg({quality: config.jpg_quality})
+      .toBuffer();
+    return `data:image/jpeg;base64,${jpgBuffer.toString('base64')}`
+  }
   const logger = ctx.logger('NRL-meme')
   const ifDebug = config.debug
   outputDebug(config);
@@ -45,21 +57,37 @@ export function apply(ctx: Context, config: Config) {
   const content_generator = async (time: number) => {
     outputDebug('content_generator启动');
     let divs = [];
-    for (let i = 0; i < time; i++) {
-      if (config.enable_convert) {
-        const response = await fetch(getRandomUrl());
-        const webpBuffer = await response.arrayBuffer();
-        const jpgBuffer = await sharp(Buffer.from(webpBuffer))
-          .jpeg({quality: config.jpgQuality})
-          .toBuffer();
-        const base64Image = `data:image/jpeg;base64,${jpgBuffer.toString('base64')}`;
-        divs.push(<message>
-          <author id="3026194904" name="Apricityx" avatar="url"/>
-          <img src={base64Image} alt="Image"/></message>);
-      } else {
-        divs.push(<message>
-          <author id="3026194904" name="Apricityx" avatar="url"/>
-          <img src={getRandomUrl()} alt={"Image"}/></message>);
+    if (config.enable_concurrent) {
+      // 并发获取图片
+      const fetchPromises = [];
+      const fetchPromise = async () => {
+        const base64Image = await getBase64Image()
+        divs.push(
+          <message>
+            <author id="3026194904" name="Apricityx" avatar="url"/>
+            <img src={base64Image} alt="Image"/>
+          </message>
+        );
+      }
+      for (let i = 0; i < time; i++) {
+        fetchPromises.push(fetchPromise());
+      }
+      logger.warn("尝试执行所有Promise");
+      await Promise.all(fetchPromises);
+    } else {
+      for (let i = 0; i < time; i++) {
+        if (config.enable_convert) {
+          // await import('node-fetch').then(async (fetch) => {
+
+          const base64Image = await getBase64Image();
+          divs.push(<message>
+            <author id="3026194904" name="Apricityx" avatar="url"/>
+            <img src={base64Image} alt="Image"/></message>);
+        } else {
+          divs.push(<message>
+            <author id="3026194904" name="Apricityx" avatar="url"/>
+            <img src={getRandomUrl()} alt={"Image"}/></message>);
+        }
       }
     }
     return divs;
